4b1cf776ccaa6d4c03328511ea6d3904d365f338
re_sh_cloud_arch <matteoalongi79@gmail.com>
2026-07-29 12:52:40 +0200

    add acr backup weu

--- src/next-core/next-core-common/99_main.tf
+++ src/next-core/next-core-common/99_main.tf
@@ -39,5 +39,5 @@ data "azurerm_client_config" "current" {}
 
 module "__v4__" {
   # v10.16.2
-  source = "git::https://github.com/pagopa/terraform-azurerm-v4?ref=0f846d5684ef493ad6d5c833d8787df3348e1326"
+  source = "git::https://github.com/pagopa/terraform-azurerm-v4?ref=372bf173e82d7ff1e705a37c1d4309e2f96ec4b6"
 }
