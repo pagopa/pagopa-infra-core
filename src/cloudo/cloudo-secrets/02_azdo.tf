0de46218be152b07cbbf0eb92d07549bb30ab01f
mamari90 <130982006+mamari90@users.noreply.github.com>
2026-07-30 16:59:10 +0200

    fix cloudo azdo access policy

--- src/cloudo/cloudo-secrets/02_azdo.tf
+++ src/cloudo/cloudo-secrets/02_azdo.tf
@@ -8,16 +8,28 @@ data "azurerm_user_assigned_identity" "iac_federated_azdo" {
   resource_group_name = local.azdo_managed_identity_rg_name
 }
 
-resource "azurerm_key_vault_access_policy" "azdevops_iac_managed_identities" {
-  for_each = local.azdo_iac_managed_identities
+# resource "azurerm_key_vault_access_policy" "azdevops_iac_managed_identities" {
+#   for_each = local.azdo_iac_managed_identities
+#
+#   key_vault_id = module.key_vault.id
+#   tenant_id    = data.azurerm_client_config.current.tenant_id
+#   object_id    = data.azurerm_user_assigned_identity.iac_federated_azdo[each.key].principal_id
+#
+#   secret_permissions = ["Get", "List", "Set", ]
+#
+#   certificate_permissions = ["SetIssuers", "DeleteIssuers", "Purge", "List", "Get"]
+#
+#   storage_permissions = []
+# }
 
-  key_vault_id = module.key_vault.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_user_assigned_identity.iac_federated_azdo[each.key].principal_id
 
-  secret_permissions = ["Get", "List", "Set", ]
-
-  certificate_permissions = ["SetIssuers", "DeleteIssuers", "Purge", "List", "Get"]
-
-  storage_permissions = []
+module "azdevops_iac_managed_identities_access_policy" {
+  source = "./.terraform/modules/__v4__/IDH/key_vault_access_policy"
+for_each = local.azdo_iac_managed_identities
+product_name         = var.prefix
+  idh_resource_tier = "devops" # or developer, external
+  env            = var.env # or prod, uat, etc.
+  key_vault_id   = module.key_vault.id
+  tenant_id      = data.azurerm_client_config.current.tenant_id
+  object_id      = data.azurerm_user_assigned_identity.iac_federated_azdo[each.key].principal_id
 }
