c48547be7a833091635d870465e83f6e0202d46b
Umberto Coppola Bottazzi <umberto.coppolabottazzi@pagopa.it>
2026-08-04 16:28:27 +0200

    feat: Bump rn-agent relase and module & set copilot secret (#45)

--- src/cloudo/cloudo-secrets/02_azdo.tf
+++ src/cloudo/cloudo-secrets/02_azdo.tf
@@ -9,13 +9,13 @@ data "azurerm_user_assigned_identity" "iac_federated_azdo" {
 }
 
 module "azdevops_iac_managed_identities_access_policy" {
-  source = "./.terraform/modules/__v4__/IDH/key_vault_access_policy"
+  source   = "./.terraform/modules/__v4__/IDH/key_vault_access_policy"
   for_each = local.azdo_iac_managed_identities
 
-  product_name         = var.prefix
-  idh_resource_tier    = "devops"
-  env                  = var.env
-  key_vault_id         = module.key_vault.id
-  tenant_id            = data.azurerm_client_config.current.tenant_id
-  object_id            = data.azurerm_user_assigned_identity.iac_federated_azdo[each.key].principal_id
+  product_name      = var.prefix
+  idh_resource_tier = "devops"
+  env               = var.env
+  key_vault_id      = module.key_vault.id
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  object_id         = data.azurerm_user_assigned_identity.iac_federated_azdo[each.key].principal_id
 }
