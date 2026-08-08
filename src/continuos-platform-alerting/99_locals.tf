9cb8d0de8ddb3c7f8e7705914556226d19ad8310
re_sh_cloud_arch <94049389+re-sh-cloud-arch@users.noreply.github.com>
2026-08-08 08:24:56 +0200

    Update 99_locals.tf

--- src/continuos-platform-alerting/99_locals.tf
+++ src/continuos-platform-alerting/99_locals.tf
@@ -4,30 +4,6 @@ locals {
   domain  = "core"
   project = "${local.product}-${var.location_short}-${local.domain}"
 
-  vnet_italy_name                = "${local.product}-itn-vnet"
-  vnet_italy_resource_group_name = "${local.product}-itn-vnet-rg"
-  vnet_core_resource_group_name  = "${local.product}-vnet-rg"
-  vnet_core_name                 = "${local.product}-vnet"
-
-  vnet_integration_resource_group_name = "${local.product}-vnet-rg"
-  vnet_integration_name                = "${local.product}-vnet-integration"
-
-  vnet_data_italy_name                = "${local.product}-itn-spoke-data-vnet"
-  vnet_data_italy_resource_group_name = "${local.product}-itn-network-hub-spoke-rg"
-
-
-  log_analytics_italy_workspace_name                = "${local.product}-itn-core-law"
-  log_analytics_italy_workspace_resource_group_name = "${local.product}-itn-core-monitor-rg"
-
-  log_analytics_weu_workspace_name                = "${local.product}-law"
-  log_analytics_weu_workspace_resource_group_name = "${local.product}-monitor-rg"
-
-  network_watcher_rg_name = "NetworkWatcherRG"
-
-  alerting_domains = ["gps", "fdr", "nodo", "crusc8", "paywallet", "core"]
-
-  azure_postgresql_resource_type = "Microsoft.DBforPostgreSQL/flexibleServers"
-  azure_redis_resource_type      = "Microsoft.Cache/Redis"
 
   monitor_resource_group_name = "${local.product}-monitor-rg"
 
